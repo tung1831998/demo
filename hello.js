708f072431d2f110394d8882df5ca190134145a3
Tùng Nguyễn Thanh <tung1831998@gmail.com>
2019-11-17 12:16:33 +0700

    User eat food

--- hello.js
+++ hello.js
@@ -2,9 +2,21 @@ var chalk = require('chalk');
 
 function user(name){
     this.name = name;
+    this.stomach =[];
 }
+
+function food(name){
+    this.name = name;
+}
+
 user.prototype.sayHi = function(){
     console.log('xin chào ' + chalk.red(this.name));
 }
 
+user.prototype.eat = function(food){
+    this.stomach.push(food);
+}
+
+
 module.exports = user;
+module.exports = food;
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var user = require("./hello");
+var food = require("./food");
+
 var user = new user('tùng');
+
 user.sayHi();
+
+var food = new food('hambuger');
+
+user.eat(food);
